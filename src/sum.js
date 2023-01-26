cf7e1756d9fb6106349529aa4f2ee18944ae6110
Yngvi Warrior <igorasft@gmail.com>
2023-01-26 17:05:55 -0300

    first

---
+++ src/sum.js
@@ -0,0 +1,17 @@
+function sum(a, b) {
+    return a + b
+}
+
+function sub(a, b) {
+    return a - b
+}
+
+function times(a, b) {
+    return a * b
+}
+
+function sumX(a, b) {
+    return a + b + a
+}
+
+module.exports = {sum, sub, times, sumX}
---
+++ src/sum.test.js
@@ -0,0 +1,5 @@
+const funcs = require('./sum')
+
+test("add 1 + 2 to be equal 3", () => {
+    expect(funcs.sum(1,2)).toBe(3)
+})
